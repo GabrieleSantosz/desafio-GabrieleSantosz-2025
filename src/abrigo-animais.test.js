a4e4b1488c39bb4f4a14064087de4c07266d7d62
GabrieleSantosz <gabrielesdazevedo@gmail.com>
2025-09-09 17:05:45 -0300

    fix test

--- src/abrigo-animais.test.js
+++ src/abrigo-animais.test.js
@@ -1,4 +1,4 @@
-import AbrigoAnimais from './abrigo-animais';  // Importando a classe com ESModules
+import AbrigoAnimais from './abrigo-animais';  
 
 
 describe('Abrigo de Animais', () => {
@@ -10,7 +10,7 @@ describe('Abrigo de Animais', () => {
 
   test('Deve encontrar pessoa para um animal', () => {
     const resultado = new AbrigoAnimais().encontraPessoas(
-      'RATO,BOLA', 'RATO,NOVELO', 'Fofo,Rex'  // Ordem invertida para bater com o teste
+      'RATO,BOLA', 'RATO,NOVELO', 'Fofo,Rex'  
     );
     expect(resultado.lista[0]).toBe('Fofo - abrigo');
     expect(resultado.lista[1]).toBe('Rex - pessoa 1');
@@ -22,13 +22,13 @@ describe('Abrigo de Animais', () => {
     const resultado = new AbrigoAnimais().encontraPessoas(
       'BOLA,LASER',
       'BOLA,NOVELO,RATO,LASER',
-      'Bola,Fofo,Mimi,Loco'  // Ordem corrigida para bater com o teste
+      'Bola,Fofo,Mimi,Loco'  
     );
 
     expect(resultado.lista[0]).toBe('Bola - abrigo');
     expect(resultado.lista[1]).toBe('Fofo - pessoa 2');
     expect(resultado.lista[2]).toBe('Mimi - pessoa 1');
-    expect(resultado.lista[3]).toBe('Loco - abrigo'); // Deve ir para o abrigo novamente
+    expect(resultado.lista[3]).toBe('Loco - abrigo'); 
     expect(resultado.lista.length).toBe(4);
     expect(resultado.erro).toBe(false);
   });
